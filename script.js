345938430bffbb2466ea27d27f26a1bdf8cf0b7c
muskanbhati7 <68704151+muskanbhati7@users.noreply.github.com>
2022-05-04 03:19:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+ const text = document.querySelector('.quote');
+ const author = document.querySelector('.author');
+ const nextBtn= document.querySelector('.next');
+ const tweetBtn = document.querySelector('.twitter-share-button')
+
+
+        const getQuote = async ()=> {
+       const res = await fetch('https://type.fit/api/quotes');
+     const quotes = await res.json();
+     const num= Math.floor(Math.random()*quotes.length);
+     console.log(num);
+     const item = quotes[num];
+     console.log(item);
+     const quote = item.text;
+     const authorName= item.author;
+     text.innerText=quote;
+     author.innerText=authorName;
+     tweetBtn.href= `https://twitter.com/intent/tweet?text=${quote}- ${author}`;
+
+
+
+}
+nextBtn.addEventListener('click',getQuote);
+getQuote()
